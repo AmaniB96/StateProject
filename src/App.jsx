15ce1f144bbcb0708caafda5ded62558d9105688
amani <bataganiramani@gmail.com>
2025-05-20 11:26:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SidePanel from './components/sidePanel'
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <SidePanel></SidePanel>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SidePanel.jsx
@@ -0,0 +1,20 @@
+import './sidePanel.css'
+
+export default function SidePanel() {
+    
+    return (   
+        <div className='panel'>
+
+            <div className='panelText'>
+                <div className='chiffre'>
+                    <p>1</p>
+                    </div>
+                <div className='steps'>
+                    <span>STEP 1</span>
+                    <p>YOUR INFO</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
