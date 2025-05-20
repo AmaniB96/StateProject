8e7b221825735d52294af78700a6fac917cd654c
amani <bataganiramani@gmail.com>
2025-05-20 13:44:58 +0200

    personal info done

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import './infos.css'
 
 export default function Infos() {
+
+    const [name,SetName] = useState("")
+    const [email, SetEmail] = useState("")
+    const [phone, SetPhone] = useState("")
 
     return (
         <div className='infos'>
@@ -11,9 +16,18 @@ export default function Infos() {
             </div>
 
             <form className='infoForm' action="">
-                
-            </form>
+                <label htmlFor="name">Your name is : {name}</label> <br />
+                <input type="text" name="" id="name" placeholder='Vingt-Six' onChange={(e) => SetName(e.target.value)} />
 
+                <label htmlFor="email">Your email is : {email}</label> <br />
+                <input type="text" name="" id="name" placeholder='Vingt-Six@email.com' onChange={(e) => SetEmail(e.target.value)}/>
+
+                <label htmlFor="phone">Your phone number is : {phone}</label> <br />
+                <input type="text" name="" id="name" placeholder='+32 000 000 000' onChange={(e) => SetPhone(e.target.value)} />
+
+                <button type="button">Next step</button>
+            </form>
+            
         </div>
     )
     
