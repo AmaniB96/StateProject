2a08ff3e442c96365f45120eef1088e651f40328
amani <bataganiramani@gmail.com>
2025-05-20 11:59:19 +0200

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Infos from './components/Infos'
 import SidePanel from './components/sidePanel'
+
 function App() {
 
   return (
     <>
     <div className='container'>
       <SidePanel></SidePanel>
+      <Infos></Infos>
     </div>
     </>
   )
---
+++ src/components/Infos.jsx
@@ -0,0 +1,15 @@
+import './infos.css'
+
+export default function Infos() {
+
+    return (
+        <div className='infos'>
+            <div className="infoTitle">
+                <h2>Personal Info</h2>
+                <span>Please provide your name, email address, and phone number.</span>
+            </div>
+
+        </div>
+    )
+    
+}
