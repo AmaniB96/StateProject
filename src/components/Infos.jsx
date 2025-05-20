2233f5278660826df1a56d9892e6489ebf283c65
amani <bataganiramani@gmail.com>
2025-05-20 12:09:50 +0200

    desing adjustmetn

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -4,10 +4,15 @@ export default function Infos() {
 
     return (
         <div className='infos'>
+
             <div className="infoTitle">
                 <h2>Personal Info</h2>
                 <span>Please provide your name, email address, and phone number.</span>
             </div>
+
+            <form className='infoForm' action="">
+                
+            </form>
 
         </div>
     )
